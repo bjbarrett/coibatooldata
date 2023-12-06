c0ea10a01b97c40a9dad4fb3f4f8b7ca6a1c07ea
mkcarlson <55852348+mkcarlson@users.noreply.github.com>
2023-12-05 19:49:00 -0800

    cleaning code with island sorting
    island sorting meredith dec 5

--- carlson_ch1_selectivity/01_kobo_cleaning_code.R
+++ carlson_ch1_selectivity/01_kobo_cleaning_code.R
@@ -7,7 +7,7 @@ library(cmdstanr)
 numbers_only <- function(x) !grepl("\\D", x)
 
 ####load new data from kobo
-dk_raw <- read.csv("~/Documents/coibatooldata/data/tool_data/coiba_capuchin_tool_survey_kobo/Capuchin_Tool_Surveying_-_all_versions_-_False_-_2023-09-21-13-51-36.csv", sep=";", header=T)
+dk_raw <- read.csv("~/Downloads/Capuchin_Tool_Surveying_-_all_versions_-_False_-_2023-09-19-14-07-47.csv", sep=";", header=T)
 # dk$hardness <- rowMeans(dk[, c("hardness_1_hld", "hardness_2_hld", "hardness_3_hld", 
 #                                "hardness_4_hld", "hardness_5_hld", "hardness_6_hld", "hardness_7_hld", 
 #                                "hardness_8_hld", "hardness_9_hld") ] , na.rm = TRUE)
@@ -26,12 +26,6 @@ coiba_days <- sort(unique(dk$today[which(dk$island=="coiba")]))
 jicaron_days <- sort(unique(dk$today[which(dk$island=="jicaron")]))
 
 
-dk$island <- ifelse( dk$today %in% coiba_days , "coiba" , "jicaron")
-dk$island_index <- as.integer(as.factor(dk$island ))
-
-dk_c <- dk[dk$island_index==1,]
-dk_j <- dk[dk$island_index==2,]
-
 xx <- as.vector(sapply(dk_raw, function(x)all(any(is.na(x))))) #makes a vector of T/F if all NA
 dk <- dk_raw[,xx==FALSE] #creat dk which removes all columns with all NAs
 colnames(dk)
@@ -48,28 +42,28 @@ dk$used_tool <- ifelse(criteria==TRUE , 1 , 0)
 
 criteria <- str_detect(dk$site_id, "Clff")  # Extract matching rows with str_detect
 for( i in 1:nrow(dk)){
-     dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
+        dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
 }
 sum(dk$used_tool)
 
 criteria <- str_detect(dk$site_id, "Fridge")  # Extract matching rows with str_detect
 for( i in 1:nrow(dk)){
-     dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
+        dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
 }
 
 criteria <- str_detect(dk$site_id, "Wtu")  # Extract matching rows with str_detect
 for( i in 1:nrow(dk)){
-     dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
+        dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
 }
 
 criteria <- str_detect(dk$site_id, "ex")  # Extract matching rows with str_detect
 for( i in 1:nrow(dk)){
-     dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
+        dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
 }
 
 criteria <- str_detect(dk$site_id, "Cebus")  # Extract matching rows with str_detect
 for( i in 1:nrow(dk)){
-     dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
+        dk$used_tool[i] <- ifelse(criteria[i]==TRUE , 1 , dk$used_tool[i])
 }
 
 dk$weight_g
@@ -98,55 +92,30 @@ astro  <- c("Astrocaryum spp, rio esc" , "Astrocaryum spp, rio esc" , "Astrocary
             "Rio escondido, fruit/nut unknown" , "Unidentified nut" , "Unknown nut: round with thin shell")
 dk_tools$astro <- 0
 for (i in 1 : nrow(dk_tools)){
-     dk_tools$astro[i] <- ifelse(dk_tools$comments[i] %in% astro , 1 , 0)
+        dk_tools$astro[i] <- ifelse(dk_tools$comments[i] %in% astro , 1 , 0)
 }
+
+#create separate data frames for each island
+dk$island <- ifelse( dk$today %in% coiba_days , "coiba" , "jicaron")
+dk$island_index <- as.integer(as.factor(dk$island ))
+dk_c <- dk[dk$island_index==1,]
+dk_j <- dk[dk$island_index==2,]
+
 ### create simplified datalist to read in models
 data_list <- list(
-     weight = dk_tools$weight_g ,
-     thickness = dk_tools$thickness ,
-     length = dk_tools$length ,
-     width = dk_tools$width ,
-     almendra = dk_tools$debris_at_site_almendra ,
-     nerite = dk_tools$debris_at_site_marine_snail ,
-     herm_crab = dk_tools$debris_at_site_hermit_crabs ,
-     halloween = dk_tools$debris_at_site_halloween_crabs ,
-     river_snail = dk_tools$debris_at_site_river_snail ,
-     bactris = dk_tools$debris_at_site_bactris_fruit , 
-     astro = dk_tools$astro
-     )
-
-data_list_raw_tc <- list(
-     weight = dk$weight_g[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     thickness = dk$thickness[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     length = dk$length[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     width = dk$width[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     almendra = dk$debris_at_site_almendra[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     nerite = dk$debris_at_site_marine_snail[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     herm_crab = dk$debris_at_site_hermit_crabs[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     halloween = dk$debris_at_site_halloween_crabs[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     river_snail = dk$debris_at_site_river_snail[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     bactris = dk$debris_at_site_bactris_fruit[dk$used_tool==0 | dk$debris_at_site_almendra==1]  , 
-     astro = dk$astro[dk$used_tool==0 | dk$debris_at_site_almendra==1] ,
-     used_tool = dk$used_tool[dk$used_tool==0 | dk$debris_at_site_almendra==1] 
+        weight = dk_tools$weight_g ,
+        thickness = dk_tools$thickness ,
+        length = dk_tools$length ,
+        width = dk_tools$width ,
+        almendra = dk_tools$debris_at_site_almendra ,
+        nerite = dk_tools$debris_at_site_marine_snail ,
+        herm_crab = dk_tools$debris_at_site_hermit_crabs ,
+        halloween = dk_tools$debris_at_site_halloween_crabs ,
+        river_snail = dk_tools$debris_at_site_river_snail ,
+        bactris = dk_tools$debris_at_site_bactris_fruit , 
+        astro = dk_tools$astro,
+        island = dk$island_index
 )
 
-str(data_list_raw_tc)
-
-dl_raw_tc_comp <- list(
-     weight_raw = dk$weight_g[dk$used_tool==0 ]  ,
-     thickness_raw = dk$thickness[dk$used_tool==0 ]  ,
-     length_raw = dk$length[dk$used_tool==0 ]  ,
-     width_raw = dk$width[dk$used_tool==0 ]  ,
-     weight_tc = dk$weight_g[dk$used_tool==1 | dk$debris_at_site_almendra==1]  ,
-     thickness_tc = dk$thickness[dk$used_tool==1 | dk$debris_at_site_almendra==1]  ,
-     length_tc = dk$length[dk$used_tool==1 | dk$debris_at_site_almendra==1]  ,
-     width_tc = dk$width[dk$used_tool==1 | dk$debris_at_site_almendra==1]  
-     # almendra = dk$debris_at_site_almendra[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     # nerite = dk$debris_at_site_marine_snail[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     # herm_crab = dk$debris_at_site_hermit_crabs[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     # halloween = dk$debris_at_site_halloween_crabs[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     # river_snail = dk$debris_at_site_river_snail[dk$used_tool==0 | dk$debris_at_site_almendra==1]  ,
-     # bactris = dk$debris_at_site_bactris_fruit[dk$used_tool==0 | dk$debris_at_site_almendra==1]  , 
-     # astro = dk$astro[dk$used_tool==0 | dk$debris_at_site_almendra==1] ,
-     #used_tool = dk$used_tool[dk$used_tool==0 | dk$debris_at_site_almendra==1] 
-)
+summary(dk_c[dk_c$used_tool==1,])
+summary(dk_c[dk_c$used_tool==0,])
