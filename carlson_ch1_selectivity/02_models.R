64795dcd57a1a617629121c78ebd0656e9a1fe9f
mkcarlson <55852348+mkcarlson@users.noreply.github.com>
2024-02-26 13:46:08 -0800

    cleaning code for island and snail
    Manually uploaded from Meredith's device
    Cleaning code for sorting by island and cleaning coiba astrocaryum and freshwater snail designations

--- carlson_ch1_selectivity/02_models.R
+++ carlson_ch1_selectivity/02_models.R
@@ -1,74 +1,132 @@
+#NEW MODELS JAN 2024 DATA AND BY ISLAND
+#jic
+#almendra vs. not almendra
+mj1 <- ulam(
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + ba*almendra,
+                a ~ normal(6,1.2), #prior for mean
+                ba ~ normal(0,1),
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list_j, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
+)
+
+#almendra and shell
+mj2 <- ulam(
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + ba*almendra + bhc*herm_crab,
+                a ~ normal(6,1.2), #prior for mean
+                c(ba,bhc) ~ normal(0,1),
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list_j, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4
+)
+
+#coiba
+#astrocaryum
+mc1 <- ulam(
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + bas*astro,
+                a ~ normal(6,1.2), 
+                bas ~ normal(0,1),
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list_c, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
+)
+
+#astro and freshwater snail
+mc2 <- ulam(
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + bas*astro + brs*river_snail,
+                a ~ normal(6,1.2), #prior for mean
+                c(bas,brs) ~ normal(0,1),
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list_c, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4
+)
+
+######
+##
+
 # intercept only model, mean of all tools
 mw0 <- ulam(
-     alist(
-          weight ~ dgamma2(mu,scale),
-          log(mu) <- a ,
-          a ~ normal(6,1.2), #prior for mean
-          scale ~ dexp(0.005)
-     ),
-     
-     data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4 
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a ,
+                a ~ normal(6,1.2), #prior for mean
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4 
 )
 
 
 #almendra vs not almendra
 mw1 <- ulam(
-     alist(
-          weight ~ dgamma2(mu,scale),
-          log(mu) <- a + ba*almendra,
-          a ~ normal(6,1.2), #prior for mean
-          ba ~ normal(0,1),
-          scale ~ dexp(0.005)
-     ),
-     
-     data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + ba*almendra,
+                a ~ normal(6,1.2), #prior for mean
+                ba ~ normal(0,1),
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
 )
 
 
 # almendra, nerite, herm_crab, hallowwen crab, river snail ignoring co-occurnce
 mw2 <- ulam(
-     alist(
-          weight ~ dgamma2(mu,scale),
-          log(mu) <- a + ba*almendra + bn*nerite + bhc*herm_crab + bgq*halloween + brs*river_snail,
-          a ~ normal(6,1.2), #prior for mean
-          c(ba,bn,bhc,bgq,brs) ~ normal(0,1),
-          scale ~ dexp(0.005)
-     ),
-     
-     data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + ba*almendra + bn*nerite + bhc*herm_crab + bgq*halloween + brs*river_snail,
+                a ~ normal(6,1.2), #prior for mean
+                c(ba,bn,bhc,bgq,brs) ~ normal(0,1),
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4
 )
 
 
 #resources permitting cooccurence
 mw3 <- ulam(
-     alist(
-          weight ~ dgamma2(mu,scale),
-          log(mu) <- a + ba*almendra + bn*nerite + bhc*herm_crab + bgq*halloween + brs*river_snail + bas*astro + INT,
-          INT <- baXn*almendra*nerite + baXhc*almendra*herm_crab + baXhcXgq*almendra*halloween*herm_crab + basXrs*astro*river_snail,
-          a ~ normal(6,1.2), #prior for mean
-          c(ba,bn,bhc,bgq,brs,baXn,baXhc,baXhcXgq,basXrs,bas) ~ normal(0,1),
-          scale ~ dexp(0.005)
-     ),
-     
-     data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + ba*almendra + bn*nerite + bhc*herm_crab + bgq*halloween + brs*river_snail + bas*astro + INT,
+                INT <- baXn*almendra*nerite + baXhc*almendra*herm_crab + baXhcXgq*almendra*halloween*herm_crab + basXrs*astro*river_snail,
+                a ~ normal(6,1.2), #prior for mean
+                c(ba,bn,bhc,bgq,brs,baXn,baXhc,baXhcXgq,basXrs,bas) ~ normal(0,1),
+                scale ~ dexp(0.005)
+        ),
+        
+        data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
 )
 
 
 #below is building up to a crazy fancy model
 mw4 <-  ulam(
-     alist(
-          weight ~ dgamma2(mu,scale),
-          log(mu) <- a + ba*almendra ,
-
-          thickness ~ dgamma2(mu_t,scale_t) ,
-          log(mu_t) <- a_t + ba_t*almendra ,
-
-          c(a,a_t) ~ normal(6,1.2), #prior for mean
-          c(ba,ba_t) ~ normal(0,1),
-          c(scale,scale_t) ~ exponential(0.005)
-     ),
-     
-     data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
+        alist(
+                weight ~ dgamma2(mu,scale),
+                log(mu) <- a + ba*almendra ,
+                
+                thickness ~ dgamma2(mu_t,scale_t) ,
+                log(mu_t) <- a_t + ba_t*almendra ,
+                
+                c(a,a_t) ~ normal(6,1.2), #prior for mean
+                c(ba,ba_t) ~ normal(0,1),
+                c(scale,scale_t) ~ exponential(0.005)
+        ),
+        
+        data=data_list, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4, 
 )
 
 #########length , width , thickness needed with rechecked priors
@@ -235,73 +293,73 @@ mt3 <- ulam(
 #####RAW MATERIAL
 
 m_raw_tc <-  ulam(
-     alist(
-          weight_tc ~ dgamma2(mu_w_tc,scale_w_tc), #unique shape and scale for almendras
-          log(mu_w_tc) <- w_tc , #the regression to which we need to make it just jicaron
-          weight_raw ~ dgamma2(mu_w_raw,scale_w_raw) , #raw weight shape and scale
-          log(mu_w_raw) <- w_raw ,
-          
-          c(w_raw,w_tc) ~ normal(5,1.8), #prior for mean
-          c(scale_w_tc,scale_w_raw) ~ exponential(0.0005) # we need a big scale for weight
-     ),
-     
-     data=dl_raw_tc_comp, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4,
+        alist(
+                weight_tc ~ dgamma2(mu_w_tc,scale_w_tc), #unique shape and scale for almendras
+                log(mu_w_tc) <- w_tc , #the regression to which we need to make it just jicaron
+                weight_raw ~ dgamma2(mu_w_raw,scale_w_raw) , #raw weight shape and scale
+                log(mu_w_raw) <- w_raw ,
+                
+                c(w_raw,w_tc) ~ normal(5,1.8), #prior for mean
+                c(scale_w_tc,scale_w_raw) ~ exponential(0.0005) # we need a big scale for weight
+        ),
+        
+        data=dl_raw_tc_comp, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4,
 )
 
 precis(m_raw_tc)
 
 m_raw_tc2 <-  ulam(
-     alist(
-          thickness_tc ~ dgamma2(mu_t_tc,scale_t_tc), #unique shape and scale for almendras
-          log(mu_t_tc) <- t_tc , #the regression to which we need to make it just jicaron
-          thickness_raw ~ dgamma2(mu_t_raw,scale_t_raw) , #raw weight shape and scale
-          log(mu_t_raw) <- t_raw ,
-          
-          c(t_raw,t_tc) ~ normal(3,2.5), #prior for mean
-          c(scale_t_tc,scale_t_raw) ~ exponential(0.01) # we need a big scale for weight
-     ),
-     
-     data=dl_raw_tc_comp, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4,
+        alist(
+                thickness_tc ~ dgamma2(mu_t_tc,scale_t_tc), #unique shape and scale for almendras
+                log(mu_t_tc) <- t_tc , #the regression to which we need to make it just jicaron
+                thickness_raw ~ dgamma2(mu_t_raw,scale_t_raw) , #raw weight shape and scale
+                log(mu_t_raw) <- t_raw ,
+                
+                c(t_raw,t_tc) ~ normal(3,2.5), #prior for mean
+                c(scale_t_tc,scale_t_raw) ~ exponential(0.01) # we need a big scale for weight
+        ),
+        
+        data=dl_raw_tc_comp, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4,
 )
 
 m_raw_tc3 <-  ulam(
-     alist(
-          weight_tc ~ dgamma2(mu_w_tc,scale_w_tc), #unique shape and scale for almendras
-          log(mu_w_tc) <- w_tc , #the regression to which we need to make it just jicaron
-          weight_raw ~ dgamma2(mu_w_raw,scale_w_raw) , #raw weight shape and scale
-          log(mu_w_raw) <- w_raw ,
-          
-          thickness_tc ~ dgamma2(mu_t_tc,scale_t_tc), #unique shape and scale for almendras
-          log(mu_t_tc) <- t_tc , #the regression to which we need to make it just jicaron
-          thickness_raw ~ dgamma2(mu_t_raw,scale_t_raw) , #raw weight shape and scale
-          log(mu_t_raw) <- t_raw ,
-          
-          length_tc ~ dgamma2(mu_l_tc,scale_l_tc), #unique shape and scale for almendras
-          log(mu_l_tc) <- l_tc , #the regression to which we need to make it just jicaron
-          length_raw ~ dgamma2(mu_l_raw,scale_l_raw) , #raw weight shape and scale
-          log(mu_l_raw) <- l_raw ,
-          
-          width_tc ~ dgamma2(mu_wd_tc,scale_wd_tc), #unique shape and scale for almendras
-          log(mu_wd_tc) <- wd_tc , #the regression to which we need to make it just jicaron
-          width_raw ~ dgamma2(mu_wd_raw,scale_wd_raw) , #raw weight shape and scale
-          log(mu_wd_raw) <- wd_raw ,
-          
-          
-          c(w_raw,w_tc) ~ normal(5,1.8), #prior for mean
-          c(scale_w_tc,scale_w_raw) ~ exponential(0.0005), # we need a big scale for weight
-          
-          c(t_raw,t_tc) ~ normal(3,2.5), #prior for mean
-          c(scale_t_tc,scale_t_raw) ~ exponential(0.01), # we need a big scale for weight
-          
-          c(l_raw,l_tc) ~ normal(3,2.5), #prior for mean
-          c(scale_l_tc,scale_l_raw) ~ exponential(0.01), # we need a big scale for weight
-          
-          c(wd_raw,wd_tc) ~ normal(3,2.5), #prior for mean
-          c(scale_wd_tc,scale_wd_raw) ~ exponential(0.01) # we need a big scale for weight
-          
-     ),
-     
-     data=dl_raw_tc_comp, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4,
+        alist(
+                weight_tc ~ dgamma2(mu_w_tc,scale_w_tc), #unique shape and scale for almendras
+                log(mu_w_tc) <- w_tc , #the regression to which we need to make it just jicaron
+                weight_raw ~ dgamma2(mu_w_raw,scale_w_raw) , #raw weight shape and scale
+                log(mu_w_raw) <- w_raw ,
+                
+                thickness_tc ~ dgamma2(mu_t_tc,scale_t_tc), #unique shape and scale for almendras
+                log(mu_t_tc) <- t_tc , #the regression to which we need to make it just jicaron
+                thickness_raw ~ dgamma2(mu_t_raw,scale_t_raw) , #raw weight shape and scale
+                log(mu_t_raw) <- t_raw ,
+                
+                length_tc ~ dgamma2(mu_l_tc,scale_l_tc), #unique shape and scale for almendras
+                log(mu_l_tc) <- l_tc , #the regression to which we need to make it just jicaron
+                length_raw ~ dgamma2(mu_l_raw,scale_l_raw) , #raw weight shape and scale
+                log(mu_l_raw) <- l_raw ,
+                
+                width_tc ~ dgamma2(mu_wd_tc,scale_wd_tc), #unique shape and scale for almendras
+                log(mu_wd_tc) <- wd_tc , #the regression to which we need to make it just jicaron
+                width_raw ~ dgamma2(mu_wd_raw,scale_wd_raw) , #raw weight shape and scale
+                log(mu_wd_raw) <- wd_raw ,
+                
+                
+                c(w_raw,w_tc) ~ normal(5,1.8), #prior for mean
+                c(scale_w_tc,scale_w_raw) ~ exponential(0.0005), # we need a big scale for weight
+                
+                c(t_raw,t_tc) ~ normal(3,2.5), #prior for mean
+                c(scale_t_tc,scale_t_raw) ~ exponential(0.01), # we need a big scale for weight
+                
+                c(l_raw,l_tc) ~ normal(3,2.5), #prior for mean
+                c(scale_l_tc,scale_l_raw) ~ exponential(0.01), # we need a big scale for weight
+                
+                c(wd_raw,wd_tc) ~ normal(3,2.5), #prior for mean
+                c(scale_wd_tc,scale_wd_raw) ~ exponential(0.01) # we need a big scale for weight
+                
+        ),
+        
+        data=dl_raw_tc_comp, cores=4 , warmup=1000 , iter=2000 , sample=TRUE, chains=4,
 )
 
 precis(m_raw_tc3)
--- carlson_ch1_selectivity/03_model interp.R
+++ carlson_ch1_selectivity/03_model interp.R
@@ -1,56 +1,27 @@
-precis(mw1) #gets summary stats of model
+precis(mj1)
+precis(mj2)
+
+precis(mc1)
+precis(mc2)
+
+##combined island models
+precis(mw1)
 precis(mw2)
 precis(mw3)
 coeftab(mw0,mw1,mw2,mw3)
 
-precis(ml1)
-precis(ml2)
-precis(ml3)
-coeftab(ml1, ml2, ml3)
-
-precis(md1)
-precis(md2)
-precis(md3)
-coeftab(md1, md2, md3)
-
-precis(mt1)
-precis(mt2)
-precis(mt3)
-coeftab(mt1, mt2, mt3)
-
 #mw0 
-post_p <-extract.prior(mw0) #extract prior from intecpts only model visualize that predictions are reasonabl given biology and also predict some unrealist values i.e. 10+ kg stones
+post_p <-extract.prior(mw0)
 precis(mw0) #summarize output
 trankplot(mw0) #diagnostics for mixing
 dev.off()
 
-#visualize prior predictive sims of intercept
-dens(data_list$weight ) # raw data
-dens(exp(post_p$a) , col="red"  ) 
-#below visualize prior
-curve(dgamma(x , shape=median(post_p$a) , scale=median(post_p$scale) ) ,  add=TRUE ,col="black")  #range of mean predictions
+dens(data_list$weight)
+dens(exp(post_p$a))
 for ( i in 1:500 ) {
-     curve(dgamma(x , shape=post_p$a[i] , scale=post_p$scale[i])  , add=TRUE ,  col=col.alpha("black",0.05))
+     curve(dgamma(x , shape=post_p$a[i] , scale=post_p$scale[i])  , add=TRUE ,  col=col.alpha("black",0.1))
+     #curve(dgamma(x , shape=(post$a[i] + post$b_adult[i]) , scale=post$scale[i] ) , add=TRUE ,  col=col.alpha("blue",0.1) , lty=1)
 }
-
-#visualize prior predictive sims with slope
-post_p <-extract.prior(mw1) #extract prior from intecpts only model visualize that predictions are reasonabl given biology and also predict some unrealist values i.e. 10+ kg stones
-precis(mw1) #summarize output
-trankplot(mw1) #diagnostics for mixing
-traceplot(mw1) #diagnostics for mixing
-
-dev.off()
-
-#visualize prior predictive sims of intercept
-dens(data_list$weight ) # raw data
-dens(exp(post_p$a) , col="red"  )
-#below visualize prior
-curve(dgamma(x , shape=median(post_p$a + post_p$ba) , scale=median(post_p$scale) ) ,  add=TRUE ,col="black") 
-
-for ( i in 1:500 ) {
-     curve(dgamma(x , shape=(post_p$a[i] + post_p$ba[i] ) , scale=post_p$scale[i])  , add=TRUE ,  col=col.alpha("black",0.05))
-}
-
 
 
 post <- extract.samples(mw0)
@@ -68,34 +39,5 @@ for ( i in 1:100 ) {
 }
 
 
-PostExtractPreds1 <- function( model , funk , almendra=0 , nerite=0 , herm_crab=0 , halloween=0 , river_snail=0 , astro=0 ){
-     post <- extract.samples(model)
-     frogs <- with(post , a + ba*almendra + bn*nerite + bhc*herm_crab + bgq*halloween + brs*river_snail + baXn*almendra*nerite + baXhc*almendra*herm_crab + baXhcXgq*almendra*halloween*herm_crab + bas*astro + basXrs*astro*river_snail,)
-     print(funk(exp(frogs)))
-}
-## raw models
-###raw_tc
-precis(m_raw_tc)
-
-
-#below are objects we can call into overleaf doc
-
-almendra_mass_mean <- PostExtractPreds1(model=mw3 , funk=mean , almendra=1)
-almendra_mass_hpdi <- PostExtractPreds1(model=mw3 , funk=HPDI , almendra=1)
-
-nerite_mass_mean <- PostExtractPreds1(model=mw3 , funk=mean , nerite=1)
-nerite_mass_hpdi <- PostExtractPreds1(model=mw3 , funk=HPDI , nerite=1)
-
-herm_crab_mass_mean  <- PostExtractPreds1(model=mw3 , funk=mean , herm_crab=1)
-herm_crab_mass_hpdi  <- PostExtractPreds1(model=mw3 , funk=HPDI , herm_crab=1)
-
-halloween_mass_mean  <- PostExtractPreds1(model=mw3 , funk=mean , halloween=1)
-halloween_mass_hpdi  <- PostExtractPreds1(model=mw3 , funk=HPDI , halloween=1)
-
-river_snail_mass_mean  <- PostExtractPreds1(model=mw3 , funk=mean , river_snail=1)
-river_snail_mass_hpdi  <- PostExtractPreds1(model=mw3 , funk=HPDI , river_snail=1)
-
-astro_mass_mean  <- PostExtractPreds1(model=mw3 , funk=mean , astro=1)
-astro_mass_hpdi  <- PostExtractPreds1(model=mw3 , funk=HPDI , astro=1)
-
+post <- extract.samples(mw3)
 
