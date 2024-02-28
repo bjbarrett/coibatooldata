cf4a06489e6920f6dbea60e1e9028a10318254a0
mkcarlson <55852348+mkcarlson@users.noreply.github.com>
2024-02-28 08:38:23 -0800

    plots for models by island
    Updated plot code for island-specific models. Manual upload from Meredith's device

--- carlson_ch1_selectivity/04_plots.R
+++ carlson_ch1_selectivity/04_plots.R
@@ -44,10 +44,10 @@ PlotToolPost(model= mw3 , river_snail=1 , color=palette[5] , graph_title="Freshw
 ###
 #mj2
 #jicaron model almendra and hermit crab
-PlotToolPost <- function( model , almendra=0 , herm_crab=0 , color="black" , graph_title="words"){
+PlotToolPost_j <- function( model , almendra=0 , herm_crab=0 , color="black" , graph_title="words"){
         post <- extract.samples(model)
         guitar <- with(post , a + ba*almendra + bhc*herm_crab)
-        dens(exp(guitar) , xlim=c(0 , 3100) , col=color , xlab="mass (g)")
+        dens(exp(guitar) , xlim=c(0 , 3100), ylim=c(0, 0.011) , col=color , xlab="mass (g)")
         shade( density(exp(guitar)) , col=col.alpha(color,0.7) , lim=PI( exp(guitar)  , prob=0.9999 ))
         scoops <- data_list_j$weight[data_list_j$almendra==almendra & data_list_j$herm_crab==herm_crab]
         points(scoops,rep(0, length(scoops)) , cex=0.7 , col=col.alpha(color,0.33) , pch=19)
@@ -55,15 +55,15 @@ PlotToolPost <- function( model , almendra=0 , herm_crab=0 , color="black" , gra
         title(main=graph_title)
 }
 
-PlotToolPost(model= mj2 , almendra=1 , color=palette[1] , graph_title= "Terminalia catappa")
-PlotToolPost(model= mj2 , herm_crab=1 , color=palette[2] , graph_title= "Hermit crab")
+PlotToolPost_j(model= mj2 , almendra=1 , color=palette[1] , graph_title= "Terminalia catappa")
+PlotToolPost_j(model= mj2 , herm_crab=1 , color=palette[2] , graph_title= "Hermit crab")
 
 #mc2
 #coiba model astrocaryum and hermit crab
-PlotToolPost <- function( model , astro=0 , river_snail=0 , color="black" , graph_title="words"){
+PlotToolPost_c <- function( model , astro=0 , river_snail=0 , color="black" , graph_title="words"){
         post <- extract.samples(model)
         nebula <- with(post , a + bas*astro + brs*river_snail)
-        dens(exp(nebula) , xlim=c(0 , 3100) , col=color , xlab="mass (g)")
+        dens(exp(nebula) , xlim=c(0 , 3100) , ylim=c(0, 0.011), col=color , xlab="mass (g)")
         shade( density(exp(nebula)) , col=col.alpha(color,0.7) , lim=PI( exp(nebula)  , prob=0.9999 ))
         scoops <- data_list_c$weight[data_list_c$astro==astro & data_list_c$river_snail==river_snail]
         points(scoops,rep(0, length(scoops)) , cex=0.7 , col=col.alpha(color,0.33) , pch=19)
@@ -71,6 +71,6 @@ PlotToolPost <- function( model , astro=0 , river_snail=0 , color="black" , grap
         title(main=graph_title)
 }
 
-PlotToolPost(model= mc2 , astro=1 , color=palette[3] , graph_title= "Astrocaryum spp")
-PlotToolPost(model= mc2 , river_snail=1 , color=palette[4] , graph_title= "Freshwater snail")
+PlotToolPost_c(model= mc2 , astro=1 , color=palette[3] , graph_title= "Astrocaryum spp")
+PlotToolPost_c(model= mc2 , river_snail=1 , color=palette[4] , graph_title= "Freshwater snail")
 
